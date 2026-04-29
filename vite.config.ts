8d1f9c4a35c052a8b7639c4148b7b237affe9bfa
tomo <europeanplaice@gmail.com>
2026-04-29 23:50:53 +0900

    Fix dev manifest asset paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const base = '/grass_puffer/'
+
 export default defineConfig({
-  base: '/grass_puffer/',
-  plugins: [react()],
+  base,
+  plugins: [
+    {
+      name: 'dev-public-asset-paths',
+      transformIndexHtml(html, context) {
+        if (!context.server) {
+          return html
+        }
+
+        return html
+          .replace(
+            /href="\/grass_puffer(?:\/grass_puffer)+\/favicon\.svg"/g,
+            'href="/grass_puffer/favicon.svg"',
+          )
+          .replace(
+            /href="\/grass_puffer(?:\/grass_puffer)+\/manifest\.webmanifest"/g,
+            'href="/grass_puffer/manifest.webmanifest"',
+          )
+          .replace(
+            /href="\/grass_puffer(?:\/grass_puffer)+\/icon\.svg"/g,
+            'href="/grass_puffer/icon.svg"',
+          )
+      },
+    },
+    react(),
+  ],
 })
