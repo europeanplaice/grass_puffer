f7acd151ca2de8f91100c116d080f4b976e8816a
tomo <europeanplaice@gmail.com>
2026-05-05 15:25:13 +0900

    Exclude unit tests from Playwright to avoid vitest/playwright conflict

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   testDir: './tests',
   fullyParallel: true,
   reporter: 'list',
+  testIgnore: ['**/unit/**'],
   webServer: {
     command: 'npm run dev',
     url: 'http://127.0.0.1:5173',
