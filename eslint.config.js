5ba7bd595489927e30d8bbdce98e18cdccdb1d88
tomo <europeanplaice@gmail.com>
2026-05-06 22:54:54 +0900

    Add ESLint security plugin

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,12 @@
 import js from "@eslint/js";
 import tseslint from "typescript-eslint";
 import reactHooks from "eslint-plugin-react-hooks";
+import security from "eslint-plugin-security";
 
 export default [
   js.configs.recommended,
   ...tseslint.configs.recommended,
+  security.configs.recommended,
   {
     files: ["src/**/*.{ts,tsx}"],
     plugins: {
